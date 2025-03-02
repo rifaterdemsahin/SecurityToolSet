0015389a9a61b85e338d5c4c61f4db19fb8cecb9
Erdem <rifaterdemsahin@gmail.com>
2025-03-02 20:42:42 +0000

    Add files via upload

---
+++ create_environment_files.sh
@@ -0,0 +1,7 @@
+touch 3.1.1_environment_XXX.md
+echo "environment: XXX Prompt: Write Pros and Cons" >> 1.1.1_objective_XXX.md
+touch 3.2.1_environment_XXX.md
+echo "environment: XXX Prompt: Write Pros and Cons" >> 1.2.1_objective_XXX.md
+touch 3.3.1_environment_XXX.md
+echo "environment: XXX Prompt: Write Pros and Cons" >> 1.3.1_objective_XXX.md
+
